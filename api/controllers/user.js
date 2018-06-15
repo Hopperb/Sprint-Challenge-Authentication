f775f6b9e3f779922cccc5c916cc3c3f3ee7a2ba
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-15 14:02:59 -0400

    -installed react app for client

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,10 +2,10 @@ const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
-  const { username, password } = req.body;
+  const {username, password} = req.body;
     User.create(req.body)
       .then(user => {
-        res.status(201).json({ username: user.username });
+        res.status(201).json({ username });
       })
       .catch(err => {
         res.status(500).json(err)
